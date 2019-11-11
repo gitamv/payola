77b1f4ca4482c3485fb83aaf470e9bccc94e37be
Ale Figueroa <afigueroa@goamv.com>
2019-11-11 14:56:27 -0600

    Rollback stripe change

--- lib/payola.rb
+++ lib/payola.rb
@@ -73,7 +73,7 @@ module Payola
     end
 
     def reset!
-      StripeEvent.event_filter = Retriever
+      StripeEvent.event_retriever = Retriever
       Stripe.api_version = ENV['STRIPE_API_VERSION'] || '2015-02-18'
 
       self.background_worker = nil
